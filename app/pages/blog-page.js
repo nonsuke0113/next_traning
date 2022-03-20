577419306662822776363f275f55d3d2340c262c
nonsuke0113 <asinal.iip36@icloud.com>
2022-03-20 10:43:12 +0900

    updat Latyout Conponent

--- app/pages/blog-page.js
+++ app/pages/blog-page.js
@@ -1,6 +1,11 @@
+import Layout from "../components/Layout"
+
 const Blog = () => {
   return (
-    <div>Blog page</div>
+    <Layout title="Blog">
+      <p className="text-4xl">Blog page</p>
+    </Layout>
+    
   )
 }
 
